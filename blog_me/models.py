36d8265a84e9e8357493664f535e84e2d2708dfc
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-03-01 00:18:33 +0300

    Refactor Blog model and templates: update fields for title, slug, short_description, and blog_body; enhance Home page to display recent articles and improve layout.

--- blog_me/models.py
+++ blog_me/models.py
@@ -19,17 +19,17 @@ STATUS_CHOICES =(
 )
 
 class Blog(models.Model):
-  title = models.CharField(max_length=100)
-  slug = models.SlugField(max_length=100, unique=True, blank=True)
+  title = models.CharField(max_length=100, unique=True, blank=True)
+  slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   is_featured = models.BooleanField(default=False)
-  short_description = models.TextField(max_length=100)
+  short_description = models.TextField(max_length=500, blank=True)
   featured_image = models.ImageField(upload_to='uplodes/%Y/%m/%d')
-  blog_body = models.TextField(max_length=5000)
+  blog_body = models.TextField(max_length=5000, blank=True)
   create_at = models.DateTimeField(auto_now_add=True)
   update_at = models.DateTimeField(auto_now=True)
-  status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Draft')
+  status = models.CharField(choices=STATUS_CHOICES, default='Draft')
   
   def __str__(self):
     return self.title
--- blog_me/views.py
+++ blog_me/views.py
@@ -4,10 +4,14 @@ from .models import Category, Blog
 
 def home(request):
   categories = Category.objects.all()
-  featured_posts = Blog.objects.filter(is_featured=True).order_by('update_at')[:5]
+  featured_posts = Blog.objects.filter(is_featured=True).order_by('update_at')
+  posts = Blog.objects.filter(is_featured=False ,status='Published').order_by('update_at')
+  
+  
   context = {
     'categories': categories,
-    'featured_posts': featured_posts
+    'featured_posts': featured_posts,
+    'posts': posts,
     
   }
   return render(request, 'Home.html', context)
