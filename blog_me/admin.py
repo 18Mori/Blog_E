fe4ad967f43b4b0f40e283122a998f7f9bb596f5
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-02-28 13:07:35 +0300

    Enhance Blog model and admin interface: add list display, search fields, and filters; rename 'auther' to 'author'; update status field to use string choices.

--- blog_me/admin.py
+++ blog_me/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Category, Blog
 
+
+class BlogAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'slug': ('title',)}
+  list_display = ['title', 'category', 'author','is_featured', 'status', 'create_at', 'update_at']
+  search_fields = ('id', 'title' ,'category__category_names', 'status',)
+  list_editable = ('is_featured', 'status') 
+  list_filter = ('status', 'category', 'author')
+
+
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
--- blog_me/models.py
+++ blog_me/models.py
@@ -14,22 +14,22 @@ class Category(models.Model):
   
 
 STATUS_CHOICES =(
-  (0, 'Draft'),
-  (1, 'Published'),
+  ('Draft', 'Draft'),
+  ('Published', 'Published'),
 )
 
 class Blog(models.Model):
   title = models.CharField(max_length=100)
   slug = models.SlugField(max_length=100, unique=True, blank=True)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
-  auther = models.ForeignKey(User, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
   is_featured = models.BooleanField(default=False)
   short_description = models.TextField(max_length=100)
   featured_image = models.ImageField(upload_to='uplodes/%Y/%m/%d')
   blog_body = models.TextField(max_length=5000)
   create_at = models.DateTimeField(auto_now_add=True)
   update_at = models.DateTimeField(auto_now=True)
-  status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+  status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Draft')
   
   def __str__(self):
     return self.title
