4557707208e4b47c1dc380c0b27e55b8a7fd4706
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-02-28 16:09:48 +0300

    Enhance blog templates and views: update Home page to display featured posts with titles and descriptions; modify base template for category navigation; improve BlogAdmin list display and filters.

--- blog_me/admin.py
+++ blog_me/admin.py
@@ -4,10 +4,10 @@ from .models import Category, Blog
 
 class BlogAdmin(admin.ModelAdmin):
   prepopulated_fields = {'slug': ('title',)}
-  list_display = ['title', 'category', 'author','is_featured', 'status', 'create_at', 'update_at']
+  list_display = ('title', 'category', 'author','status','is_featured')
   search_fields = ('id', 'title' ,'category__category_names', 'status',)
   list_editable = ('is_featured', 'status') 
-  list_filter = ('status', 'category', 'author')
+  list_filter = ('status', 'category', 'author', 'is_featured')
 
 
 admin.site.register(Category)
--- blog_me/views.py
+++ blog_me/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Blog
 
 
 def home(request):
-  return render(request, 'Home.html')
+  categories = Category.objects.all()
+  featured_posts = Blog.objects.filter(is_featured=True).order_by('update_at')[:5]
+  context = {
+    'categories': categories,
+    'featured_posts': featured_posts
+    
+  }
+  return render(request, 'Home.html', context)
