33d90e6dc9e5264650999b0f2ef58dad3b9e68a3
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-03-01 15:28:51 +0300

    Add category-based post listing: implement posts_by_category view and template; update URL routing for category posts.

--- blog_me/urls.py
+++ blog_me/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='Home')
+    path('', views.home, name='Home'),
+    path('<int:category_id>/', views.posts_by_category, name='post_by_category')
 ]
--- blog_me/views.py
+++ blog_me/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Blog
-
+from django.http import HttpResponse
 
 def home(request):
   categories = Category.objects.all()
@@ -15,3 +15,13 @@ def home(request):
     
   }
   return render(request, 'Home.html', context)
+
+
+def posts_by_category(request, category_id):
+  posts =Blog.objects.filter(category=category_id,status='Published').order_by('update_at')
+  category = get_object_or_404(Category, id=category_id)
+  context = {
+    'category' : category,
+    'posts' : posts,
+  }
+  return render(request, 'post_by_category.html', context=context)
